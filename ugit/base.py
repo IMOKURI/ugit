3eac18102ffe84dc850e9fd330b4a657e3050efc
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2020-09-30 12:53:14 +0900

    commit: Record hash of last commit to HEAD

--- ugit/base.py
+++ ugit/base.py
@@ -78,11 +78,15 @@ def read_tree(tree_oid):
 
 
 def commit(message):
-    commit = f"tree {write_tree ()}\n"
+    commit = f"tree {write_tree()}\n"
     commit += "\n"
     commit += f"{message}\n"
 
-    return data.hash_object(commit.encode(), "commit")
+    oid = data.hash_object(commit.encode(), "commit")
+
+    data.set_HEAD(oid)
+
+    return oid
 
 
 def is_ignored(path):
