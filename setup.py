b1562674f76181313267aa45e45332c79ec32b6b
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2020-09-30 10:56:23 +0900

    ugit: DIY Git in Python

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="ugit",
+    version="0.1",
+    packages=["ugit"],
+    entry_points={"console_scripts": ["ugit = ugit.cli:main"]},
+)
